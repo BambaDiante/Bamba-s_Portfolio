1ad3d670cdaf219b8ed2805b5d7f03289dd5c4a6
BambaDiante <ahmadoubambadiante4@gmail.com>
2026-08-19 17:10:24 +0000

    Modification du header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,20 @@
 import { Link } from 'react-router-dom'
+import profilImage from '../assets/profil.png'
 
 
 function Header(){
     return (
         <div>
             <header className="portfolio-header">
-                <p className="eyebrow">Portfolio personnel</p>
-                <h1>Bamba Diante</h1>
-                <p className="intro">Des projets concrets, des interfaces utiles et une stack choisie avec intention.</p>
+                <div className="portfolio-header-copy">
+                    <h1>Développeur FullStack</h1>
+                    <br />
+                    <h3>Ahmadou Bamba Diante</h3>
+                    <p className="intro">Des projets concrets, des interfaces utiles et une stack choisie avec intention.</p>
+                </div>
+                <div className="portfolio-header-portrait">
+                    <img src={profilImage} alt="Photo de profil de Bamba Diante" className="profile-image" />
+                </div>
             </header>
         </div>
     )
