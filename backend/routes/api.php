41aab2f9a01f7684db379d735ec2cd58ff4a2bd0
BambaDiante <ahmadoubambadiante4@gmail.com>
2026-08-24 20:06:20 +0000

    Fix sanctum

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,20 +7,18 @@ use App\Http\Controllers\CompetenceController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ContactController;
 
-
-
-
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::get('/projects', [ProjectsController::class, 'index']);
-Route::get('/competences',[CompetenceController::class,'index']);
-Route::get('/about',[AdminController::class,'getbio']);
-Route::get('/parcours',[AdminController::class,'getparcours']);
-Route::post('/contact',[ContactController::class,'store']);
+Route::get('/competences', [CompetenceController::class, 'index']);
+Route::get('/about', [AdminController::class, 'getbio']);
+Route::get('/parcours', [AdminController::class, 'getparcours']);
+Route::post('/contact', [ContactController::class, 'store']);
 
-Route::middleware(['auth'])->prefix('admin')->group(function () {
+// Utiliser 'auth:sanctum' au lieu de 'auth' pour les routes API SPA
+Route::middleware(['auth:sanctum'])->prefix('admin')->group(function () {
 
     // Projets
     Route::post('/create/project', [ProjectsController::class, 'store']);
@@ -38,6 +36,5 @@ Route::middleware(['auth'])->prefix('admin')->group(function () {
     Route::delete('/skill/{id}', [CompetenceController::class, 'destroySkill']);
 
 });
-
 
 require __DIR__.'/auth.php';
